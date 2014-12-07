d812ef47df8ff2d7299c1bf0855f9b37c82c3b4a
Nathan <nhall@hmc.edu>
2014-12-06 20:00:51 -0800

    Added semantics class, have yet to pass it to parser

--- documents/SingleAIParserImplementation.py
+++ documents/SingleAIParserImplementation.py
@@ -7,3 +7,64 @@ class SingleAIGrammarParserImplementation(SingleAIGrammarParser):
             nameguard=nameguard,
             **kwargs
         )
+
+class IQException(Exception):
+    pass
+
+class SingleAIGrammarSemantics(object):
+    maxDumbRules = 2
+    maxMediumRules = 4
+    maxSmartRules = 6
+
+    def __init__(self):
+        self.dumbRules = 0
+        self.mediumRules = 0
+        self.smartRules = 0
+
+    def dumbFormation(self, ast):
+        self.dumbRules += 1
+        if self.dumbRules > maxDumbRules:
+            raise IQException("Too many rules supplied to short AI")
+        return ast
+    def dumbMovement(self, ast):
+        self.dumbRuleS += 1
+        if self.dumbRules > maxDumbRules:
+            raise IQException("Too many rules supplied to short AI")
+        return ast
+    def dumTargeting(self, ast):
+        self.dumbRules += 1
+        if self.dumbRules > maxDumbRules:
+            raise IQException("Too many rules supplied to short AI")
+        return ast
+
+    def mediumFormation(self, ast):
+        self.mediumRules += 1
+        if self.mediumRules > maxMediumRules:
+            raise IQException("Too many rules supplied to int AI")
+        return ast
+    def mediumMovement(self, ast):
+        self.mediumRules += 1
+        if self.mediumRules > maxMediumRules:
+            raise IQException("Too many rules supplied to int AI")
+        return ast
+    def mediumTargeting(self, ast):
+        self.mediumRules += 1
+        if self.mediumRules > maxMediumRules:
+            raise IQException("Too many rules supplied to int AI")
+        return ast
+
+    def smartFormation(self, ast):
+        self.smartules += 1
+        if self.smartules > maxSmartRules:
+            raise IQException("Too many rules supplied to double AI")
+        return ast
+    def smartMovement(self, ast):
+        self.smartules += 1
+        if self.smartules > maxSmartRules:
+            raise IQException("Too many rules supplied to double AI")
+        return ast
+    def smartTargeting(self, ast):
+        self.smartules += 1
+        if self.smartules > maxSmartRules:
+            raise IQException("Too many rules supplied to double AI")
+        return ast
